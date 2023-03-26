ca9a2f295f2c3933514560998be64d6e62e8ce09
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-03-26 08:42:40 +0545

    orderItemsController missed function outline created

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -1,11 +1,24 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func GetOrderItems() gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 	}
+}
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func ItemsByOrder(id string) (OrderItems []primitive.M, err error) {
+
 }
 
 func GetOrderItem() gin.HandlerFunc {
